73a4d5974c6c5be0101ddcefb9e55ade042e65c7
Francis AbouNader <francis.abounader1999@gmail.com>
2021-08-30 11:05:28 +0300

    Latest update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HoursWeekPicker from "./HoursWeekPicker";
+
 class App extends React.Component {
   render() {
     return (
--- src/Hours.js
+++ src/Hours.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+
 export default function Hours() {
   const time = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
